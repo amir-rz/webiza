9f021f7d75e3ce46ad766743a149187628d706f8
amir-rz <mr.amir.rezazadehh@gmail.com>
2021-06-30 00:30:51 +0430

    Coming soon

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <Page dotBackdrop size="mini">
       <Page.Header>
-        <Text h2 style={{fontWeight:"bold"}}>Webiza test</Text>
+        <Text h2 style={{fontWeight:"bold"}}>Webiza</Text>
       </Page.Header>
       <Text>
-       Time to have your modern website.
+       Coming soon...
       </Text>
     </Page>
   )
