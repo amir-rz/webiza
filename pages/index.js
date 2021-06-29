689a2dfe7bf5297d37a126dc1a4d564617e4a2cb
amir-rz <mr.amir.rezazadehh@gmail.com>
2021-06-29 19:13:42 +0430

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import { Page, Text } from '@geist-ui/react'
+
+export default function Home() {
+  return (
+    <Page dotBackdrop size="mini">
+      <Page.Header>
+        <Text h2 style={{fontWeight:"bold"}}>Webiza</Text>
+      </Page.Header>
+      <Text>
+       Time to have your modern website.
+      </Text>
+    </Page>
+  )
+}
